50ceb5fe1b62d3b1fdc313fb0343d09b0c38609a
getBrandon <brandonrussellshaffer@gmail.com>
2022-07-19 21:54:31 -0400

    Added Command Line Runner to make an admin on startup.

--- src/main/java/com/palaceflophouse/supportportal/SupportPortalApplication.java
+++ src/main/java/com/palaceflophouse/supportportal/SupportPortalApplication.java
@@ -1,13 +1,31 @@
 package com.palaceflophouse.supportportal;
 
+import com.palaceflophouse.supportportal.entities.User;
+import com.palaceflophouse.supportportal.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SupportPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportPortalApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner addAdminUserOnStartup(UserRepository userRepository, PasswordEncoder encoder){
+		String encodedPassword = encoder.encode("password1");
+		User admin = User.builder()
+				.username("admin")
+				.password(encodedPassword)
+				.firstName("admin")
+				.lastName("user")
+				.build();
+		admin.setIsAdmin(true);
+		return args -> userRepository.save(admin);
 	}
 
 }
