48e50898c9921a54cb5ddcffe33c87d869dedceb
getBrandon <brandonrussellshaffer@gmail.com>
2022-08-13 22:22:25 -0400

    Adding accounts to the admin console.

--- src/main/java/com/palaceflophouse/supportportal/controller/AdminConsoleController.java
+++ src/main/java/com/palaceflophouse/supportportal/controller/AdminConsoleController.java
@@ -1,14 +1,15 @@
 package com.palaceflophouse.supportportal.controller;
 
+import com.palaceflophouse.supportportal.entities.Account;
 import com.palaceflophouse.supportportal.entities.User;
+import com.palaceflophouse.supportportal.repository.AccountRepository;
 import com.palaceflophouse.supportportal.repository.UserRepository;
+import com.palaceflophouse.supportportal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * Author: Brandon Shaffer
@@ -19,15 +20,22 @@ import java.util.List;
 public class AdminConsoleController {
 
 	public final UserRepository userRepository;
+	public final AccountRepository accountRepository;
 
 	@Autowired
-	public AdminConsoleController(UserRepository userRepository){
+	public AdminConsoleController(UserRepository userRepository, UserService userService, AccountRepository accountRepository){
 		this.userRepository = userRepository;
+		this.accountRepository = accountRepository;
 	}
 
 	@ModelAttribute("users")
 	public Iterable<User> users(){
 		return userRepository.findAll();
+	}
+
+	@ModelAttribute("accounts")
+	public Iterable<Account> accounts(){
+		return accountRepository.findAll();
 	}
 
 	@GetMapping
